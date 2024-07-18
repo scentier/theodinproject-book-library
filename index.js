4f20259a5c43390d07c6ec728f4b4b7faddb4307
unknown <142760953+scentier@users.noreply.github.com>
2024-07-18 18:00:16 +0700

    add remove basic

--- index.js
+++ index.js
@@ -16,19 +16,28 @@ addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("The Little Prince", "Antoine de Saint-Exupéry", 96, true);
 addBookToLibrary("Murder on the Orient Express", "Agatha Christie", 274, false);
 
-let bookContent = "";
-for (let index = 0; index < myLibrary.length; index++) {
-  bookContent += "<ul>";
-  bookContent += `<li class="title">${myLibrary[index].title}</li>`;
-  bookContent += `<li class="author">${myLibrary[index].author}</li>`;
-  bookContent += `<li class="pages">${myLibrary[index].pages}</li>`;
-  bookContent += `<li class="read">${
-    myLibrary[index].read ? "read" : "not read"
-  }</li>`;
-  bookContent += "</ul>";
+function displayBooks(bookList) {
+  let bookContent = "";
+  for (let index = 0; index < bookList.length; index++) {
+    bookContent += `<div class="index-${index}" data-index-number="${index}">`;
+    bookContent += "<ul>";
+    bookContent += `<li class="title">${bookList[index].title}</li>`;
+    bookContent += `<li class="author">${bookList[index].author}</li>`;
+    bookContent += `<li class="pages">${bookList[index].pages}</li>`;
+    bookContent += `<li class="read">${
+      bookList[index].read ? "read" : "not read"
+    }</li>`;
+    bookContent += "</ul>";
+    bookContent += `<button type="button" onclick="removeBook(${index})">Remove Book</button>`;
+    bookContent += "</div>";
+  }
+  return (document.getElementById("book-list").innerHTML = bookContent);
 }
 
-document.getElementById("book-list").innerHTML = bookContent;
+function removeBook(bookIndex) {
+  myLibrary.splice(bookIndex, 1);
+  displayBooks(myLibrary);
+}
 
 const addNewBook = document.getElementById("addNewBook");
 const dialog = document.getElementById("dialogNewBook");
@@ -45,16 +54,17 @@ cancel.addEventListener("click", function () {
 
 submitBook.addEventListener("click", function (event) {
   event.preventDefault();
-
   const newTitle = document.getElementById("bookTitle").value;
   const newAuthor = document.getElementById("bookAuthor").value;
   const newPages = document.getElementById("bookPages").value;
   const newRead = document.getElementById("bookRead").checked;
-
   addBookToLibrary(newTitle, newAuthor, newPages, newRead);
-  console.log(newTitle);
-  console.log(newAuthor);
-  console.log(newPages);
-  console.log(newRead);
+  displayBooks(myLibrary);
   dialog.close();
+  document.getElementById("bookTitle").value = "";
+  document.getElementById("bookAuthor").value = "";
+  document.getElementById("bookPages").value = "";
+  document.getElementById("bookRead").checked = false;
 });
+
+displayBooks(myLibrary);
